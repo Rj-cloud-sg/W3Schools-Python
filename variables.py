65d843d6e8f40492971986f5f6321f54e268cbc1
Rj-cloud-sg <rjspencer07@icloud.com>
2026-01-16 16:06:37 -0500

    W3Schools Added variable names exercise

---
+++ variables.py
@@ -0,0 +1,32 @@
+print("variables in python do not need a type to be set")
+
+x = 5
+x = "RJ"
+print(x)
+
+print("I can set a type by casting", end=" using str(), int(), float(), etc "); 
+
+x = str(5)
+y = int(8)
+z = float(10) 
+print(x, y, z)
+
+print("I can also get the typ of any variable using the type() function ");
+
+h = "100"
+print(type(h))
+print(type(y))
+
+print("VARIABLE NAMES")
+print("Acceptable variable names", end=" myDog, MyCar, my_money, test_Case_1, MyMomsDocs.")
+myDog = "Fido"
+MyCar = "Toyota"
+my_money = 1000
+test_Case_1 = True
+MyMomsDocs = ["doc1", "doc2", "doc3"]
+
+print(myDog)
+print(MyCar)
+print(my_money)
+print(test_Case_1)
+print(MyMomsDocs)
