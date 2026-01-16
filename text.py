bd0a0f33c6fa7a3df87759219bf27de3c87b0eee
Rj-cloud-sg <rjspencer07@icloud.com>
2026-01-16 15:42:49 -0500

    W3Schools text exercise and practice with number

--- text.py
+++ text.py
@@ -1,1 +1,5 @@
-print("This text shows how to write code on the same line.", end=" Otherwise this sentence would've been on a separate line")
+print("This text shows how to write code on the same line.", end=" Otherwise this sentence would've been on a separate line.")
+
+print(" Now I will practice with numbers"); print("I am", 20, "years old"); 
+print("My football jersey number is", 2 + 3,)
+print("I'm on year", 1 * 3, "of college")
