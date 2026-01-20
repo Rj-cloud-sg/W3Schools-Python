fa51638577bcf97b026aee97091ee445a848a66c
Rj-cloud-sg <rjspencer07@icloud.com>
2026-01-20 18:49:54 -0500

    Data types exercise

---
+++ dataTypes.py
@@ -0,0 +1,24 @@
+print("Get the data type with the Type() function")
+
+x = "RJ"
+print(type(x))
+
+y = 5.0
+print(type(y))
+
+House = ["Car", "Stairs", "Dog"]
+print(type(House))
+
+classRoomSupplies  = ("Pencil", "Eraser", "Notebook")
+print(type(classRoomSupplies))
+
+print("You can also set a specific data type")
+
+x = str("Hello World")
+print(type(x))
+
+y = int(20)
+print(type(y))
+
+z = float(5.5)
+print(type(z))
