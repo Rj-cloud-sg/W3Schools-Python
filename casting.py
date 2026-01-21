9bc5d66fcddf1c62ee122ee44da8e8d1ce9022de
Rj-cloud-sg <rjspencer07@icloud.com>
2026-01-20 19:15:05 -0500

    Casting exercise

---
+++ casting.py
@@ -0,0 +1,17 @@
+print("casting is used when you want to specify a variable type")
+
+x = str("3")    # x will be '3'
+y = int(3.56)    # y will be 3
+z = int(3.8)  # z will be 3
+
+print(x, y, z)
+
+a = float(5)    # a will be 5.0
+b = float("7.2")  # b will be 7.2
+
+print(a, b)
+
+e = str(10.5)  # e will be '10.5'
+f = str(100)    # f will be '100'
+
+print(e, f)
